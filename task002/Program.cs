e4d10465a31dfdad7cb151cee9e9d8c5e3423b5e
DanilaPestov <89147750075@mail.ru>
2022-04-19 23:30:06 +1000

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,7 @@
+﻿//рандомное число
+int numberA = new Random().Next(10, 100);
+Console.WriteLine("наше число: " + numberA);
+int b = numberA % 10;
+int c = numberA / 10;
+if (b > c) Console.WriteLine("наибольшее цифра: " + b);
+else Console.WriteLine("наибольшая цифра: " + c);
