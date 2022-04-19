910f65aa66137051ee3b04f206aa1f53a911d71f
DanilaPestov <89147750075@mail.ru>
2022-04-19 23:30:24 +1000

    task003

---
+++ task003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int result = a / b;
+if (a % b == 0)
+{
+    Console.WriteLine("результат: " + result);
+}
+else
+{
+    int result2 = a % b;
+    Console.WriteLine("не кратно, остаток " + result2);
+}
