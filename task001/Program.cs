5000cee824eb8de92d78957fd12ff4f0578db5c9
DanilaPestov <89147750075@mail.ru>
2022-04-19 23:28:45 +1000

    task001

---
+++ task001/Program.cs
@@ -0,0 +1,15 @@
+﻿//последняя цифра числа
+Console.WriteLine("введите число: ");
+
+int num = int.Parse(s: Console.ReadLine());
+
+Console.WriteLine("последняя цифра: " + num % 10);
+
+//вторая цифра числа
+Console.WriteLine("введите трехзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+number = number % 100;
+number = number / 10;
+Console.WriteLine($"вторая цифра: " + number);
