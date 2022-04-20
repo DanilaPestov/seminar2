ee23f5b5b44c0e1a23fb0ab42e041e3cec4a888d
DanilaPestov <89147750075@mail.ru>
2022-04-20 23:25:41 +1000

    task004

---
+++ task004/task004.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("1 число: ");
+int a = int.Parse(Console.ReadLine());
+
+int b = 7;
+int c = 21;
+
+if ((a % b == 0) & (a % c == 0))   // "и"
+//if ((a % b == 0) | (a % c == 0))   // "или"
+{
+    Console.WriteLine("кратно 21 и 7");
+}
+else
+{
+    Console.WriteLine("НЕ кратно 21 и 7");
+}
